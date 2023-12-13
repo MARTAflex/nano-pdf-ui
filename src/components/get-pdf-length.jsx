c764b8864dfc9613007fee589c3b07617dc6310e
throo <throo@protonmail.com>
2023-12-13 14:07:36 +0100

    added form to select pdf to test

--- src/components/get-pdf-length.jsx
+++ src/components/get-pdf-length.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import { Button, Row, Col, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 import testPdfs from '../assets/test-pdfs.json';
@@ -10,34 +10,46 @@ const client = axios.create({
 });
 
 export const GetPDFLength = (ps) => {
-    const [data, setData] = useState({});
+    const [selectedPdf, setSelectedPdf] = useState('to-text');
+    const [responseData, setResponseData] = useState({});
 
     const getPdfLength = async () => {
         client
             .post('/get-pdf-length', {
-                pdf: testPdfs['to-text'], //pdf is expected to be encoded as base64
+                pdf: testPdfs[selectedPdf], //pdf is expected to be encoded as base64
             })
             .then((response) => {
-                setData(response?.data);
+                setResponseData(response?.data);
             })
             .catch((err) => {
-                setData(err);
+                setResponseData(err);
             });
     };
 
+    
     return (
         <Row>
             <Col xs={3}>
                 <h2>get-pdf-length</h2>
+                <Form.Group>
+                    <Form.Select
+                        value={selectedPdf}
+                        onChange={ (next) => setSelectedPdf(next.target.value)}
+                        >
+                        { Object.keys(testPdfs).map( (key,i) => (
+                            <option value={key} key={i}>{key}</option>
+                            ))}
+                    </Form.Select>
+                </Form.Group>
                 <Button onClick={() => getPdfLength()}>send request</Button>
-                <Button variant='danger' onClick={() => setData({})}>
+                <Button variant='danger' onClick={() => setResponseData({})}>
                     x
                 </Button>
             </Col>
             <Col>
                 <h3>Response:</h3>
                 <div className='bg-light border p-3'>
-                    <pre>{JSON.stringify(ejson(data), null, 4)}</pre>
+                    <pre>{JSON.stringify(ejson(responseData), null, 4)}</pre>
                 </div>
             </Col>
         </Row>
