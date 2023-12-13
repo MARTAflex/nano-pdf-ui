7a51304945e314cd76d25672c84b5ecb087e32f8
throo <throo@protonmail.com>
2023-12-13 12:27:16 +0100

    pathfinder 'get-pdf-length' works now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+import testPdfs from "./assets/test-pdfs.json";
+
+const client = axios.create({
+    baseURL: "/nano-pdf",
+});
 
 function App() {
+    const [data, setData] = useState(null);
+
+    const getPdfLength = async () => {
+        client
+            .post("/get-pdf-length",{
+                pdf: testPdfs["to-text"], //pdf is expected to be encoded as base64
+            }, )
+            .then((response) => {
+                setData(response?.data)
+            })
+            .catch((err) => { 
+                setData(err)
+            });
+    };
 
     return (
         <div>
-            asd
+            <h2>get-pdf-length</h2>
+            <button onClick={() => getPdfLength()}>send request</button>
+            <div>
+                <pre>
+                    {JSON.stringify(data)}
+                </pre>
+            </div>
         </div>
     );
 }
