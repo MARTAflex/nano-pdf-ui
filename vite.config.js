87c4288a547db4b35137830ec264fcf3bea5e130
throo <throo@protonmail.com>
2023-12-13 12:26:54 +0100

    configured vite proxy to nano-pdf instance

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server:{
+        proxy: {
+            "/nano-pdf": {
+                target: "http://localhost:9091",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/nano-pdf/, ""),
+            }
+        }
+    },
     plugins: [react()],
 });
